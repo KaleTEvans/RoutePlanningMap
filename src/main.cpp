238d5883b195807091fbba7e0df652345229ada1
Kale Evans <kaletevans@gmail.com>
2021-09-30 11:30:29 -0500

    small changes

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 #include "render.h"
 #include "route_planner.h"
 
-using namespace std::experimental;
+using namespace std;
 
 static std::optional<std::vector<std::byte>> ReadFile(const std::string &path)
 {   
@@ -52,20 +52,18 @@ int main(int argc, const char **argv)
             osm_data = std::move(*data);
     }
     
-    // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
-    // user input for these values using std::cin. Pass the user input to the
-    // RoutePlanner object below in place of 10, 10, 90, 90.
+    // gather start and end points
     float start_x, start_y, end_x, end_y;
 
-    std::cout << "Enter the start and end coordinates in the following order" << endl;
-    std::cout << "X Start Value" << endl;
-    std::cin >> start_x;
-    std::cout << endl << "Y Start Value" << endl;
-    std::cin >> start_y;
-    std::cout << endl << "X End Value" << endl;
-    std::cin >> end_x;
-    std::cout << endl << "Y End Value" << endl;
-    std::cin >> end_y;
+    cout << "Enter the start and end coordinates in the following order" << endl;
+    cout << "X Start Value" << endl;
+    cin >> start_x;
+    cout << endl << "Y Start Value" << endl;
+    cin >> start_y;
+    cout << endl << "X End Value" << endl;
+    cin >> end_x;
+    cout << endl << "Y End Value" << endl;
+    cin >> end_y;
 
 
     // Build Model.
